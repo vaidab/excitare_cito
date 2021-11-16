01c8d2c0bc98cc35a43960d5263d077a19836357
vaidab <contact@vaidabogdan.com>
2021-11-16 22:22:35 +0200

    Fixed INFURA ID key.

--- config.py
+++ config.py
@@ -15,4 +15,4 @@ SLEEP_TIME_SECONDS = 1  # check only once per 120 seconds until we're closer to 
 MAX_DECIMALS = 2  # 2 is more visually appealing than 16
 
 token_abi = abi.token_abi
-INFURA_API_KEY = "c7f51e44ade649e7bbe03f6ca64a7a14"  # for ETH and ROPSTEN networks
+INFURA_API_KEY = ""  # for ETH and ROPSTEN networks
